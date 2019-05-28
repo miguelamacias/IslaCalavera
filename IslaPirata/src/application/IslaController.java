f3b30ad9246cea2bbe21cac813368f549113eb09
Admin <Admin@PC-Angel>
2019-05-28 21:24:31 +0200

    Versión preliminar del selector de dados terminada.
    Hay que refactorizar bastante e implementar el fin de juego por trío de
    calaveras.

--- IslaPirata/src/application/IslaController.java
+++ IslaPirata/src/application/IslaController.java
@@ -2,6 +2,7 @@ package application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -12,6 +13,7 @@ public class IslaController {
 	Tirada tirada;
 	ImageView[] imagenes;
 	Boolean[] dadosMarcados;
+	CheckBox[] checkboxes;
 	
 	//Declaración de variables para el SceneBuilder
 	@FXML
@@ -44,6 +46,29 @@ public class IslaController {
 	@FXML
 	private Label resultado;
 	
+	@FXML
+	private CheckBox chkDado1;
+	
+	@FXML
+	private CheckBox chkDado2;
+	
+	@FXML
+	private CheckBox chkDado3;
+	
+	@FXML
+	private CheckBox chkDado4;
+	
+	@FXML
+	private CheckBox chkDado5;
+	
+	@FXML
+	private CheckBox chkDado6;
+	
+	@FXML
+	private CheckBox chkDado7;
+	
+	@FXML
+	private CheckBox chkDado8;
 	
 	
 	@FXML
@@ -52,27 +77,19 @@ public class IslaController {
 		dados = new Dado[9];
 		tirada = new Tirada();
 		imagenes = new ImageView[9];
-		crearArrayImageViews();
+		checkboxes = new CheckBox[9];
+		inicializarArrays();
 		
 		//Crea la array de los 8 dados del juego
 		for (int i = 1; i < dados.length; i++) {
 			dados[i] = new Dado();
-		}
-		
-		//Crea la array de dados a jugar
-		dadosMarcados = new Boolean[9];
-		dadosMarcados[1] = true;
-		dadosMarcados[2] = true;
-		dadosMarcados[3] = true;
-		dadosMarcados[4] = true;
-		dadosMarcados[5] = true;
-		dadosMarcados[6] = true;
-		dadosMarcados[7] = true;
-		dadosMarcados[8] = true;
+		}	
 	}
 	
 	@FXML
 	void tirar(ActionEvent event) {
+		marcarDados();
+		
 		//Realiza una tirada y calcula los puntos obtenidos
 		int puntos = tirada.calcularPuntuacion(tirada.obtenerTirada(dados, dadosMarcados));
 		resultado.setText(String.valueOf(puntos));
@@ -83,12 +100,20 @@ public class IslaController {
 			Image imagen = new Image(rutaImagen);
 			imagenes[i].setImage(imagen);
 		  }
-		
-		dadosMarcados[7] = false;
-		dadosMarcados[8] = false;
 	}
 	
-	private void crearArrayImageViews() {
+	private void marcarDados() {
+		dadosMarcados[1] = chkDado1.isSelected();
+		dadosMarcados[2] = chkDado2.isSelected();
+		dadosMarcados[3] = chkDado3.isSelected();
+		dadosMarcados[4] = chkDado4.isSelected();
+		dadosMarcados[5] = chkDado5.isSelected();
+		dadosMarcados[6] = chkDado6.isSelected();
+		dadosMarcados[7] = chkDado7.isSelected();
+		dadosMarcados[8] = chkDado8.isSelected();
+	}
+	
+	private void inicializarArrays() {
 		//Crea una array con todos los imageviews de los dados
 		imagenes[1] = img1;
 		imagenes[2] = img2;
@@ -99,6 +124,27 @@ public class IslaController {
 		imagenes[7] = img7;
 		imagenes[8] = img8;
 		
+		//Crea la array de dados a jugar
+		dadosMarcados = new Boolean[9];
+		dadosMarcados[1] = true;
+		dadosMarcados[2] = true;
+		dadosMarcados[3] = true;
+		dadosMarcados[4] = true;
+		dadosMarcados[5] = true;
+		dadosMarcados[6] = true;
+		dadosMarcados[7] = true;
+		dadosMarcados[8] = true;	
+		
+		//Crea la array de checkboxes
+		
+		checkboxes[1] = chkDado1;
+		checkboxes[2] = chkDado2;
+		checkboxes[3] = chkDado3;
+		checkboxes[4] = chkDado4;
+		checkboxes[5] = chkDado5;
+		checkboxes[6] = chkDado6;
+		checkboxes[7] = chkDado7;
+		checkboxes[8] = chkDado8;
 	}
 	
 }
