c21c74aa3d7f42aed7b0d21e1e8cc4ace9c15982
Macías <miguel.a.macias.13@gmail.com>
2019-06-07 19:49:59 +0200

    Version 1.0 Beta 3.1
    -Limpiados import no usados
    
    TODO:
    -Datos de conexión BD en properties.
    -Refactorizar clase IslaController.

--- IslaPirata/src/application/test.java
+++ IslaPirata/src/application/test.java
@@ -1,17 +1,10 @@
 package application;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 
 public class test {
 
 	public static void main(String[] args) throws Exception {
-		//Class.forName("org.mariadb.jdbc.Driver");
-		
-		String fecha = "2019-06-03 12:33:22.0";
-		
-		//String fechaFormateada = String.format(%s, args)
+		//Ninguna prueba en curso
 	}
 
 }
