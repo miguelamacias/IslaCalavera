5712c46ff554f6f1a2b6fae19e432683fdc3d4e4
Macías <miguel.a.macias.13@gmail.com>
2019-08-20 17:29:28 +0200

    Versión con cartas terminadas.

--- IslaPirata/src/application/Carta.java
+++ IslaPirata/src/application/Carta.java
@@ -3,12 +3,12 @@ package application;
 import java.security.SecureRandom;
 
 public class Carta {
-	private enum Valores {ORO, DIAMANTE, BRUJA, COFRE, PIRATA, CALAVERA, DOBLE_CALAVERA, BARCO_2X, BARCO_3X, BARCO_4X, MONO_LORO};
+	private enum Valores {ORO, DIAMANTE, COFRE, PIRATA, CALAVERA, DOBLE_CALAVERA, MONO_LORO, BRUJA, BARCO_2X, BARCO_3X, BARCO_4X};
 	private Valores valor;
 	
 	public String sacarCarta() {
 		SecureRandom generadorAleatorio = new SecureRandom();
-		valor = Valores.values()[generadorAleatorio.nextInt(11)];
+		valor = Valores.values()[generadorAleatorio.nextInt(7)]; //Cuando se implementen la bruja y barcos cambiar el número por un 11.
 		return valor.name();
 	}
 	
