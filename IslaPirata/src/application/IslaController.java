82466504332dbdb421862a6aad2cf0dff63f8833
Macías <miguel.a.macias.13@gmail.com>
2019-08-27 12:35:18 +0200

    Nueva tabla finalizada

--- IslaPirata/src/application/IslaController.java
+++ IslaPirata/src/application/IslaController.java
@@ -7,7 +7,6 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
-import javafx.scene.control.ScrollBar;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.ScrollPane.ScrollBarPolicy;
 import javafx.scene.control.TextInputDialog;
